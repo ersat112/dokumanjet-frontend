a9434d3de31c62af37e3a98dd92111bef7b6a1bb
ersat112 <ersat.bulbul@gmail.com>
2025-04-25 17:20:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,22 @@
+// vite.config.js
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // @ ile src klasörüne erişim
+    },
+  },
+  server: {
+    port: 5173, // Lokal geliştirme portu
+    open: true, // Tarayıcıyı otomatik açar
+  },
+  build: {
+    outDir: "dist", // Vercel deploy klasörü
+    emptyOutDir: true,
+  },
+});
