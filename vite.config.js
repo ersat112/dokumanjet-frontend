99b9356ed654a357977966f4168e6c363ae0599c
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 00:25:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,24 @@
 // vite.config.js
-
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  // Ortam değişkenlerini tanımla
   define: {
     'process.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL = "https://dokumanjet-backend.onrender.com/api/v1"
+      process.env.VITE_API_URL || "https://dokumanjet-backend.onrender.com/api/v1"
     ),
   },
-});
-export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // @ ile src klasörüne erişim
+      "@": path.resolve(__dirname, "./src"), // '@' ile src klasörüne erişim
     },
   },
   server: {
     port: 5173, // Lokal geliştirme portu
-    open: true, // Tarayıcıyı otomatik açar
+    open: true, // Tarayıcı otomatik açılır
   },
   build: {
     outDir: "dist", // Vercel deploy klasörü
