519f2be8c233f38dd5ad370998703cd8e2f2bbd1
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 01:06:42 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,23 @@
+// src/utils/api.js
 
+import axios from "axios";
+import { VITE_API_URL, DEFAULT_TIMEOUT } from "../config";
+
+const api = axios.create({
+  baseURL: VITE_API_URL,
+  timeout: DEFAULT_TIMEOUT,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Token eklemek için interceptor
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+export default api;
