cab297093cac2f955ece77eb3093211367c7bcc4
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 21:17:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
