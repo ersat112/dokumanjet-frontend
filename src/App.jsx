4482e45e8baf3fe8de29cd011a546c7b3abd070b
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 22:53:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Favorites from './pages/Favorites';
+import Header from '@/components/Header';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+import Favorites from '@/pages/Favorites';
 
 export default function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
