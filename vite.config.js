9e18bb143bdf828b7b69cae7cc0a2e17ee1060ef
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 22:32:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -13,16 +15,21 @@ export default ({ mode }) => {
         '/api': {
           target: apiUrl,
           changeOrigin: true,
-          secure: false,
-        },
-      },
+          secure: false
+        }
+      }
     },
     resolve: {
-      alias: { '@': '/src' },
+      alias: { '@': '/src' }
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer()]
+      }
     },
     build: {
       outDir: 'dist',
-      emptyOutDir: true,
-    },
+      emptyOutDir: true
+    }
   });
 };
