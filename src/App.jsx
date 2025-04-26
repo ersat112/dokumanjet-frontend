077d6bf65d06a2f56fcad3ebb1dc4158cd973af3
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 21:08:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Favorites from "./pages/Favorites";
-import Header from "./components/Header";
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Favorites from './pages/Favorites';
 
-function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-
-  const handleLogin = (newToken) => {
-    setToken(newToken);
-    localStorage.setItem("token", newToken);
-  };
-
-  const handleLogout = () => {
-    setToken(null);
-    localStorage.removeItem("token");
-  };
-
-  useEffect(() => {
-    const savedToken = localStorage.getItem("token");
-    if (savedToken) {
-      setToken(savedToken);
-    }
-  }, []);
-
+export default function App() {
   return (
     <Router>
-      <Header token={token} onLogout={handleLogout} />
       <Routes>
-        <Route path="/" element={<Home token={token} />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route path="/register" element={<Register onLogin={handleLogin} />} />
-        <Route path="/favorites" element={<Favorites token={token} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
