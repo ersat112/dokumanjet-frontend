4310e6a03178b330d93cbb90b8a4930c72626e31
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 21:21:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./main.css"; // Eğer bir ana css dosyan varsa
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
