268d46a5b0972899e95f5b11f62955ee084df714
ersat112 <ersat.bulbul@gmail.com>
2025-04-26 22:17:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const apiUrl = env.VITE_API_URL;
+  const apiUrl = env.VITE_API_URL || 'https://dokumanjet-backend.onrender.com/api/v1';
 
   return defineConfig({
     plugins: [react()],
-    publicDir: 'public',
     server: {
       port: 3000,
       proxy: {
         '/api': {
           target: apiUrl,
           changeOrigin: true,
-          secure: false
-        }
-      }
+          secure: false,
+        },
+      },
     },
     resolve: {
-      alias: { '@': '/src' }
+      alias: { '@': '/src' },
     },
     build: {
       outDir: 'dist',
-      emptyOutDir: true
-    }
+      emptyOutDir: true,
+    },
   });
 };
-
